0668e8ec84c0ebd49c6f8e301835facf18c7e1ad
Remitito <jrossot@protonmail.com>
2022-03-11 11:31:03 +0800

    Loan processing added

---
+++ server/LoanLedger.java
@@ -0,0 +1,24 @@
+package newbank.server;
+
+import java.util.ArrayList;
+
+public class LoanLedger {
+    private ArrayList<Loan> loans;
+
+    public LoanLedger() {
+        this.loans = new ArrayList<Loan>();
+    }
+
+    public Loan getLoan(String ID) {
+        for (int i = 0; i < this.loans.size(); i++) {
+            if(loans.get(i).getLoanId() == ID) {
+                return loans.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void addLoan(Loan loan) {
+        this.loans.add(loan);
+    }
+}

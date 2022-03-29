01d7cc3c4da427165bf828702bb26e8bd2823f59
owenant <anthonyowen1@gmail.com>
2022-03-29 10:14:29 +0100

    corrected messaging to client

--- server/NewBankClientHandler.java
+++ server/NewBankClientHandler.java
@@ -8,8 +8,10 @@ import java.io.FileReader;
 //------------------------------------------------------
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class NewBankClientHandler extends Thread{
 	
@@ -19,11 +21,13 @@ public class NewBankClientHandler extends Thread{
 	private BufferedReader in_auto_checkin;
 	//------------------------------------------------------
 	private PrintWriter out;
+	private Scanner myScanner;
 	
 	
 	public NewBankClientHandler(Socket s) throws IOException {
 		bank = bank.getBank();
 		in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		myScanner = new Scanner(in);
 		//------Auto-check-in code - can be deleted later-------
 		try {
 			in_auto_checkin = new BufferedReader(
@@ -40,32 +44,37 @@ public class NewBankClientHandler extends Thread{
 	public void run() {
 		// keep getting requests from the client and processing them
 		try {
-			// ask for user name
-			out.println("Enter Username");
-			//------Updated Auto-check-in code - can be reverted later-------
-			//String userName = in.readLine();
-			String userName = in_auto_checkin.readLine();
-			//---------------------------------------------------------------
-			// ask for password
-			out.println("Enter Password");
-			//------Updated Auto-check-in code - can be reverted later-------
-			//String password = in.readLine();
-			String password = in_auto_checkin.readLine();
-			//---------------------------------------------------------------
-			out.println("Checking Details...");
-			// authenticate user and user object from bank for use in subsequent requests
-			User user = bank.checkLogInDetails(userName, password);
+			User loggedInUser = runLogIn();
 			// if the user is authenticated then get requests from the user and process them 
-			if(user != null) {
+			if(loggedInUser != null) {
+				//display initial interface
 				out.println("Log In Successful.");
-				interfaceDisplay(user);
+				interfaceDisplay(loggedInUser);
+				//get user requests
 				while(true) {
 					String request = in.readLine();
-					System.out.println("Request from " + user.getUserID().getKey());
-
-
-					String response = bank.processRequest(user, request);
-					out.println(response);
+					System.out.println("Request from " + loggedInUser.getUserID().getKey());
+					//select option chosen by user
+					if (loggedInUser.getUserType().equals("customer")) {
+						switch (request) {
+							//needs to be maintained in sync with request files
+							case "1":  showMyAccounts_Interface(loggedInUser); break;
+							case "2" : depositCash_Interface(loggedInUser); break;
+							case "3" : withdrawCash_Interface(loggedInUser); break;
+							case "4":  Loan_Interface(loggedInUser); break;
+							//case "5" : repay loan
+				    default:
+							System.out.println("FAIL");
+						}
+					}else if(loggedInUser.getUserType().equals("bank manager")){
+						switch (request) {
+							//needs to be maintained in sync with request files
+							//case "1": return createAccount_Interface(); break;
+							//case "2" :return deleteAccount_Interface(); break;
+							default:
+								System.out.println("FAIL");
+						}
+					}
 				}
 			}
 			else {
@@ -83,6 +92,95 @@ public class NewBankClientHandler extends Thread{
 				Thread.currentThread().interrupt();
 			}
 		}
+	}
+
+	//interface and input collection functions for handling client IO
+	private User runLogIn(){
+		String userName = "";
+		String password = "";
+		try {
+			// ask for user name
+			out.println("Enter Username");
+			//------Updated Auto-check-in code - can be reverted later-------
+			//userName = in.readLine();
+			userName = in_auto_checkin.readLine();
+			//---------------------------------------------------------------
+			// ask for password
+			out.println("Enter Password");
+			//------Updated Auto-check-in code - can be reverted later-------
+			// password = in.readLine();
+			password = in_auto_checkin.readLine();
+			//---------------------------------------------------------------
+			out.println("Checking Details...");
+		}catch (IOException e){
+			System.out.println("Log-in failed.");
+			System.exit(0);
+		}
+
+		// authenticate user and user object from bank for use in subsequent requests
+		User user = bank.checkLogInDetails(userName, password);
+		return user;
+	}
+
+	private void showMyAccounts_Interface(User user){
+		String response = bank.showMyAccounts_process(user);
+		out.println(response);
+	}
+
+	private void depositCash_Interface(User user){
+		//ask customer to choose account
+		out.println("Select the account you wish to deposit cash:");
+		Customer cust = (Customer) user;
+		ArrayList<Account> custAccounts = cust.getAccounts();
+		for(int i = 0; i < custAccounts.size(); i++) {
+			out.println((i + 1) + " - " + custAccounts.get(i).toString()); // added 1 sp
+		}
+		int selection = myScanner.nextInt();
+
+		//ask for deposit amount
+		out.println("Enter the deposit amount");
+		Double depositAmt = myScanner.nextDouble();
+
+		String response = bank.depositCash_process(depositAmt,
+				custAccounts.get(selection-1).getAccountNum(), cust);
+		out.println(response);
+	}
+
+	private void withdrawCash_Interface(User user){
+		//ask customer to choose account
+		out.println("Select the account you wish to withdraw cash from:");
+		Customer cust = (Customer) user;
+		ArrayList<Account> custAccounts = cust.getAccounts();
+		for(int i = 0; i < custAccounts.size(); i++) {
+			out.println((i + 1) + " - " + custAccounts.get(i).toString()); // added 1 sp
+		}
+		int selection = myScanner.nextInt();
+
+		//ask for deposit amount
+		out.println("Enter the withdrawal amount");
+		Double withdrawalAmt = myScanner.nextDouble();
+
+		String response = bank.withdrawCash_process(withdrawalAmt,
+				custAccounts.get(selection-1).getAccountNum(), cust);
+		out.println(response);
+	}
+
+	private void Loan_Interface(User user) {
+		Customer cust = (Customer) user;
+		ArrayList<Account> accounts = cust.getAccounts();
+		// User chooses an account
+		out.println("Select the account from which you wish to request a loan");
+		for(int i = 0; i < accounts.size(); i++) {
+			out.println((i + 1) + " - " + accounts.get(i).toString()); // added 1 sp
+		}
+		int account = myScanner.nextInt();
+
+		// User enters loan amount
+		out.println("Enter the requested loan amount");
+		Double amount = myScanner.nextDouble();
+
+		String response = bank.Loan_process(amount, accounts.get(account-1).getAccountNum());
+		out.println(response);
 	}
 
 	//function to provide a nicer interface on the command line
@@ -106,5 +204,4 @@ public class NewBankClientHandler extends Thread{
 			out.println(intKey.toString() + "." + process);
 		}
 	}
-
 }
